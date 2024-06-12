ea8b067f2eca3238f0a298e4e1b2e2895b36123c
inmaBD <inma.ortiztabar@gmail.com>
2024-06-12 10:56:20 +0200

    Delete comandos/lanzar_varios.bat

--- comandos/lanzar_varios.bat
+++
@@ -1,3 +0,0 @@
-yolo train data=dataset.yaml obb model=yolov8m-obb.pt project=pruebaM epochs=400 patience=60 imgsz=512 batch=-1  dropout=0.3 
-yolo train data=dataset.yaml obb model=yolov8l-obb.pt project=pruebaL epochs=400 patience=60 imgsz=512 batch=-1  dropout=0.3  
-yolo train data=dataset.yaml obb model=yolov8x-obb.pt project=pruebaX epochs=400 patience=60 imgsz=512 batch=-1  dropout=0.3  
